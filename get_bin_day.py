622b3a06d8b590d40ea1d080e3a483b68db194ec
Chris01s <cowens21@qub.ac.uk>
2019-02-23 14:30:12 +0000

    Changed bin_collection.py

--- get_bin_day.py
+++ get_bin_day.py
@@ -1,8 +1,13 @@
 #!/bin/python
 
-
+import sys
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+from prettytable import PrettyTable
+
+addressLookup = sys.argv[1]
+city = sys.argv[2]
+postcode = sys.argv[3]
 
 ##open browser and got to the form web page
 driver = webdriver.Chrome() 
@@ -15,7 +20,7 @@ element.click()
 
 ##send text to the search box
 textbox = driver.find_element_by_xpath("//input[@name='Postcode_textbox']")
-textbox.send_keys("BT9 7AY")
+textbox.send_keys(postcode)
 
 ##click submit button
 look_up_address = driver.find_element_by_name('AddressLookup_button')
@@ -23,16 +28,18 @@ look_up_address.click()
 
 ##select address from drop down list
 select = Select(driver.find_element_by_name("lstAddresses"))
-select.select_by_visible_text('Apartment 2,2A Dunluce Avenue, Belfast, BT9 7AY')
+select.select_by_visible_text(addressLookup+", "+city+", "+postcode)
 
 ##get results from the table
-results = driver.find_element_by_id("binsGrid") 
-columns = results.find_elements_by_tag_name('th') 
-values = results.find_elements_by_tag_name('td') 
 
-##print results
-for col,val in zip(columns,values):
-   print col.text+":",val.text
+results = driver.find_elements_by_tag_name("tr") 
+column_headers = [col.text.strip() for col in results[0].find_elements_by_tag_name('th')]
+table_of_results = PrettyTable(column_headers)
+
+for result in results[1:]:
+   values = [res.text for res in result.find_elements_by_tag_name('td')]
+   table_of_results.add_row(values)
    
+print (table_of_results)  
 ##close driver
 driver.close()
